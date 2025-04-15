7304e6f6d85e24f72e3ddd91672d4fb7ad36538d
Lauritaweb <laheavy@gmail.com>
2025-04-15 16:30:02 -0300

    mega menu index

---
+++ nav.php
@@ -0,0 +1,126 @@
+<div class="marquee bg-black text-white text-center">
+        <p>
+            <i class="bi bi-bicycle"></i>
+            de ciclistas para ciclistas...
+        </p>
+    </div>
+    <!-- navbar -->
+    <nav class="navbar navbar-expand-lg  position-relative pt-5">
+        <div class="container">
+      
+          <!-- Botón Hamburguesa -->
+          <button class="navbar-toggler me-auto" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+      
+          <!-- Logo Centrado Absoluto -->
+          <a class="navbar-brand position-absolute top-50 start-50 translate-middle" href="./index.html">
+            <img src="./assets/img/logo-forever-bikes.svg" alt="Forever Bikes" >
+          </a>
+      
+          <!-- Menú principal -->
+          <div class="collapse navbar-collapse justify-content-between" id="navbarNav">
+      
+            <!-- Links a la izquierda -->
+            <ul class="navbar-nav me-auto">
+              <li class="nav-item dropdown position-static">
+                <a class="nav-link dropdown-toggle" href="#" id="megaMenuLink" data-bs-toggle="dropdown">
+                  PRODUCTOS
+                </a>
+                <div class="dropdown-menu w-100 mt-0 p-4 border-0 shadow-lg">
+                  <div class="container">
+                    <div class="row">
+                      <div class="col-md-2">
+                        <h6 class="fw-bold">Bicis</h6>
+                        <ul class="list-unstyled">
+                          <li><a class="dropdown-item" href="./productos/bicis/bicis-urbanas.html">Urbanas</a></li>
+                          <li><a class="dropdown-item" href="./productos/bicis/bicis-ruta.html">Ruta</a></li>
+                          <li><a class="dropdown-item" href="./productos/bicis/bicis-todo-terreno.html">Todo terreno</a></li>
+                          <li><a class="dropdown-item" href="./productos/bicis/mi-primera-forever.html">Mi primera Forever</a></li>
+                          <li><a class="dropdown-item text-key" href="./productos/bicis/index.html">Ver todas</a></li>
+                        </ul>
+                      </div>
+                      <div class="col-md-2">
+                        <h6 class="fw-bold">Componentes</h6>
+                        <ul class="list-unstyled">
+                          <li><a class="dropdown-item" href="./productos/componentes/asientos.html">Asientos</a></li>
+                          <li><a class="dropdown-item" href="./productos/componentes/frenos.html">Frenos</a></li>
+                          <li><a class="dropdown-item" href="./productos/componentes/cuadros.html">Cuadros</a></li>
+                          <li><a class="dropdown-item" href="./productos/componentes/stems.html">Stems</a></li>
+                          <li><a class="dropdown-item" href="./productos/componentes/manubrios.html">Manubrios</a></li>
+                          <li><a class="dropdown-item" href="./productos/componentes/ruedas-componentes.html">Ruedas y componentes</a></li>
+                          <li><a class="dropdown-item" href="./productos/componentes/transmicion.html">Trasmición</a></li>
+                          <li><a class="dropdown-item text-key" href="./productos/componentes/index.html">Ver todos</a></li>
+                        </ul>
+                      </div>
+                      <div class="col-md-2">
+                        <h6 class="fw-bold">Accesorios</h6>
+                        <ul class="list-unstyled">
+                          <li><a class="dropdown-item" href="./productos/accesorios/cascos.html">Cascos</a></li>
+                          <li><a class="dropdown-item" href="./productos/accesorios/bolsos.html">Bolsos</a></li>
+                          <li><a class="dropdown-item" href="./productos/accesorios/luces.html">Luces</a></li>
+                          <li><a class="dropdown-item" href="./productos/accesorios/ciclocomputadores.html">Ciclocomputadores</a></li>
+                          <li><a class="dropdown-item" href="./productos/accesorios/herramientas.html">Herramientas</a></li>
+                          <li><a class="dropdown-item" href="./productos/accesorios/infladores.html">Infladores</a></li>
+                          <li><a class="dropdown-item" href="./productos/accesorios/lingas.html">Lingas</a></li>
+                          <li><a class="dropdown-item" href="./productos/accesorios/punos-cintas.html">Puños / Cintas</a></li>
+                          <li><a class="dropdown-item" href="./productos/accesorios/otros.html">Otros</a></li>
+                          <li><a class="dropdown-item text-key" href="./productos/accesorios/index.html">Ver todos</a></li>
+                        </ul>
+                      </div>
+                      <div class="col-md-2">
+                        <h6 class="fw-bold">Indumentaria</h6>
+                        <ul class="list-unstyled">
+                          <li><a class="dropdown-item text-key" href="./productos/indumentaria/index.html">Ver todos</a></li>
+                        </ul>
+                      </div>
+                      <div class="col-md-2">
+                        <h6 class="fw-bold">Servicios</h6>
+                        <ul class="list-unstyled">
+                          <li><a class="dropdown-item text-key" href="./productos/cursos/index.html">Ver todos</a></li>
+                        </ul>
+                      </div>
+                      <div class="col-md-2">
+                        <h6 class="fw-bold">Cursos</h6>
+                        <ul class="list-unstyled">
+                          <li><a class="dropdown-item text-key" href="./productos/cursos/index.html">Ver todos</a></li>
+                        </ul>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </li>
+              <li class="nav-item dropdown position-static">
+                <a class="nav-link dropdown-toggle" href="#" id="megaMenuLink" data-bs-toggle="dropdown">
+                    SERVICES
+                    </a>
+                <div class="dropdown-menu w-100 mt-0 p-4 border-0 shadow-lg">
+                  <div class="container">
+                    <div class="row">
+                      <div class="col-md-2">
+                            <h6 class="fw-bold">Cursos</h6>
+                            <ul class="list-unstyled">
+                                <li><a class="dropdown-item text-key" href="./productos/cursos/index.html">Ver todos</a></li>
+                            </ul>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </li>
+              <li class="nav-item"><a class="nav-link" href="#our-bikes">NUESTRAS BICIS</a></li>
+              <li class="nav-item"><a class="nav-link" href="./about-foreverbikes.html">SOMOS FOREVER</a></li>
+              <li class="nav-item"><a class="nav-link" href="#forever-comunity">COMUNIDAD</a></li>
+            </ul>
+      
+            <!-- Iconos a la derecha -->
+            <div class="d-flex gap-3 align-items-center">
+              <a href="./checkout.html" class="nav-link border-end pe-3"><i class="bi bi-cart"></i></a>
+              <a href="https://www.instagram.com/foreverbikesargentina/" class="nav-link"><i class="bi bi-instagram"></i></a>
+              <a href="https://www.youtube.com/@foreverbikesargentina9001" class="nav-link"><i class="bi bi-youtube"></i></a>
+              <a href="https://www.facebook.com/Foreverbikesarg" class="nav-link"><i class="bi bi-facebook"></i></a>
+              <a href="https://www.tiktok.com/@foreverbikesargentina" class="nav-link"><i class="bi bi-tiktok"></i></a>
+            </div>
+          </div>
+        </div>
+      </nav>
+    <!-- end navbar -->
